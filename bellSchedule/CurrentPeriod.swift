48faf81b18a5ec60d9fcd1950f78813f28d4b24d
Ari Stassinopoulos <aris@qsysmine.tk>
2016-12-07 06:22:20 -0800

    Init commit

---
+++ bellSchedule/CurrentPeriod.swift
@@ -0,0 +1,30 @@
+//
+//  CurrentPeriod.swift
+//  bellSchedule
+//
+//  Created by Ari Stassinopoulos on 8/13/16.
+//  Copyright © 2016 Stassinopoulos. All rights reserved.
+//
+
+import Foundation
+public class CurrentPeriod {
+    public var periodOffset:Int;
+    public var isCurrentPeriod:Bool;
+    public init() {
+        let currentDate = Date();
+        let currentSchedule = CurrentSchedule().currentSchedule;
+        var offset = 0;
+        isCurrentPeriod = false;
+        for period in currentSchedule {
+            let start = period.0;
+            let end = period.1;
+            if(start < currentDate && end > currentDate) {
+                isCurrentPeriod = true;
+                break;
+            } else {
+                offset += 1;
+            }
+        }
+        periodOffset = offset;
+    }
+}
