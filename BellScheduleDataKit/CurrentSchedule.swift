3088c54ac74fa87c79b6328668577e5c9a92c1d5
Ari Stassinopoulos <aris@qsysmine.tk>
2017-09-04 06:04:47 -0700

    Init commit

--- BellScheduleDataKit/CurrentSchedule.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public class CurrentSchedule {
-	public var currentSchedule:[(Date, Date, String)];
-	public init() {
-		let currentTimings = CurrentTimings().currentTimings;
-		currentSchedule = Schedule(timings: currentTimings).asDates;
-	}
-}
